ad38aa8ca0b0b49653683111f56116275f6e813d
bun <khoanguyendang325@gmail.com>
2026-06-04 09:26:46 +0700

    capnhat

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,14 @@
-/*viết mã javascript*/
+/*Viết mã Javascript*/
+function printBCC(n)
+{
+    let result = "";
+
+    let i = 1;
+    while(i<=10)
+    {
+        result += `${n} x ${i} = ${n*i} <br>`; 
+        i++;
+    }
+    //Đưa kết quả vào thẻ div có id là "result"
+    document.getElementById("result").innerHTML = result;
+}
